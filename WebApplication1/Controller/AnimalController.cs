f9a1bc4a3231e9e1dd1daeead25a34c48aff0474
nevader <krzysiek.przybysz8@gmail.com>
2024-04-14 13:42:37 +0200

    Initial commit

---
+++ WebApplication1/Controller/AnimalController.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnimalController : ControllerBase
+{
+    private readonly string _connectionString = "Server=db-mssql16.pjwstk.edu.pl;Database=Test;User Id=Test;Password=Test;";
+    
+    [HttpGet]
+    public IActionResult GetAnimals (string orderBy = "name")
+    {
+        var connection = new SqlConnection(_connectionString);
+        var command = new SqlCommand($"SELECT * FROM Animals ORDER BY {orderBy}", connection);
+        
+    }
+    
+}
---
+++ WebApplication1/Model/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Model;
+
+public class Animal
+{
+    public int idAnimal { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string Area { get; set; }
+    
+    public Animal(int idAnimal, string name, string description, string category, string area)
+    {
+        this.idAnimal = idAnimal;
+        Name = name;
+        Description = description;
+        Category = category;
+        Area = area;
+    }
+}   
